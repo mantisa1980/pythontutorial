5324f6e7d351568633865d93ec196e7aa3c7cc61
duyhsieh <duyhsieh@igs.com.tw>
2017-08-02 09:40:21 +0800

    modify refernece

--- src/practice/reference.py
+++ src/practice/reference.py
@@ -6,14 +6,14 @@ def bar(i):
     return i
 
 def qux(x):
-    x.append(999)
+    x.append(4)
     return x
 
 # immutable objects
 x = 123
-print "before:x={}, id(x)={}".format(x, id(x))
+print "before bar:x={}, id(x)={}".format(x, id(x))
 y = bar(x)
-print "after :x={}, y={},id(x)={}, id(y)={}".format(x,y,id(x),id(y))
+print "after bar:x={}, y={},id(x)={}, id(y)={}".format(x,y,id(x),id(y))
 print "id(x)==id(123)? {} , id(y)==id(124)? {}".format(id(x)==id(123), id(y)==id(124))
 print "x is123? {} , y is 124? {}".format(x is 123, y is 124)
 
@@ -22,3 +22,5 @@ a = [1,2,3]
 b = qux(a)
 print "a={},b={}".format(a , b)
 
+c = [1,2,3,4]
+print "a == c ? {}; a is c ? {}".format(a==c, a is c)
