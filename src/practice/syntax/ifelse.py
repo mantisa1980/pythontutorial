5df15f098ed17eeec308377e97201d1ad522a4bc
duyhsieh <mantisa1980@gmail.com>
2018-08-02 23:48:21 +0800

    add loop sample

---
+++ src/practice/syntax/ifelse.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+'''
+remove first line and test the result
+'''
+
+'''
+this is a block comment
+'''
+
+# This is a line comment
+
+while True:
+    x = int(input('Please input age number\n'))
+    if x < 10:
+        print ("Age is {}, you are kid".format(x))
+    elif x  < 18:
+        print ("Age is {}, you are teenager".format(x))
+    elif x  < 30:
+        print ("Age is {}, you are young man".format(x))
+    elif x  < 45:
+        print ("Age is {}, you are grown man".format(x))
+    else:
+        print ("Age is {}, you are old man".format(x))
+
